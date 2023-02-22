1c2063e72f07468dc6230b49af9385a4ad8f1ed4
GulyasAnita <anitagulyas72@gmail.com>
2023-02-22 14:48:47 +0100

    gyakorlas

---
+++ Gyakorlas.java
@@ -0,0 +1,29 @@
+
+package gyakorlas;
+
+
+public class Gyakorlas {
+
+  
+    public static void main(String[] args) {
+        
+    }
+    
+    private static int elso10SzamOsszege(){
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg +=i;
+        }
+        return osszeg;
+        //System.out.println("Az első 10 szám összege: " + osszeg);
+    }
+    private static int osszead(int  a, int b){
+    return a+b;    
+    //System.out.printf("%d + %d =%d\n ",a,b,a+b);
+    }
+    private static void kiir(String szoveg) {
+        System.out.println(szoveg);
+    }
+    
+}
+
